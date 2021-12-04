9ea1ec905592d621f47120f19272d47e56e0596a
Daniel Maslowski <info@orangecms.org>
2021-12-04 02:53:16 +0100

    fixup

--- arch/riscv/mm/init.c
+++ arch/riscv/mm/init.c
@@ -935,6 +935,8 @@ int __meminit vmemmap_populate(unsigned long start, unsigned long end, int node,
 #endif
 
 #ifdef CONFIG_64BIT
+struct __riscv_pbmt_struct __riscv_pbmt __ro_after_init;
+EXPORT_SYMBOL(__riscv_pbmt);
 struct riscv_custom_pte __riscv_custom_pte __ro_after_init;
 EXPORT_SYMBOL(__riscv_custom_pte);
 #endif
