2661eedc0266f430dfeaf10f0e518a8df0d2cbb9
Daniel Maslowski <info@orangecms.org>
2023-08-13 19:14:51 +0200

    move DT to even higher offset - we need space now, sorry
    Signed-off-by: Daniel Maslowski <info@orangecms.org>

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ DTB=$BDIR/dts/sun8i-v40-sharevdi-r1-256m.dtb
 # NOTE: Arm default text offset is 0x8000, see  `linux/arch/arm/Makefile`
 KER_ADDR=0x40008000
 # 10 MB offset for DTB
-DTB_ADDR=0x40c08000
+DTB_ADDR=0x40f08000
 
 echo Chip ID:
 $FEL sid
